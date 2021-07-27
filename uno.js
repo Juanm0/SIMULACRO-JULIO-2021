51e75133e3a8e22253d929156c60559c1a71d805
unknown <juanm.navarro05@gmail.com>
2021-07-27 20:48:00 -0300

    Simulacro

--- uno.js
+++ uno.js
@@ -1,3 +1,4 @@
+//Navarro Juan Div H Modelo de parcial
 /*Nos dedicamos a la venta exclusiva de Discos rígidos.
 Debemos realizar la carga de 5(cinco) productos, de cada uno debo obtener los 
 siguientes datos:
@@ -15,5 +16,61 @@ c) Cuántas unidades de HDD hay en total.*/
 
 function mostrar()
 {
-	alert("uno");
+	var contadorProductos;
+	var tipoDeProducto;
+	var precioDeProducto;
+	var cantidadDeUnidades;
+	var marcaDeProducto;
+	var capacidadDisco;
+	var acumuladorHDD;
+	var acumuladorSSD;
+	var acumuladorSSDM2;
+
+	contadorProductos=0;
+	contadorProductos=parseInt(contadorProductos);
+	acumuladorHDD=0;
+	acumuladorHDD=parseInt(acumuladorHDD);
+	acumuladorSSD=0;
+	acumuladorSSD=parseInt(acumuladorSSD);
+
+	while (contadorProductos<5)
+	{
+		tipoDeProducto=prompt("Hola, solo disponemos de HDD, SSD Y SSM2. Ingrese el producto que desea comprar");
+		while(tipoDeProducto!="HDD"&&tipoDeProducto!="SSD"&&tipoDeProducto!="SSDM2")
+		{
+			tipoDeProducto=prompt("Error.. Ingrese un producto valido (HDD, SSD Y SSDM2).");
+		}
+		precioDeProducto=prompt("Ingrese el precio de su "+tipoDeProducto);
+		precioDeProducto=parseInt(precioDeProducto);
+		while(isNaN(precioDeProducto)&&precioDeProducto<5000&&precioDeProducto>18000)
+		{
+			precioDeProducto=prompt("Error.. Ingrese un precio valido (entre 5000 y 18000)");
+		}
+		cantidadDeUnidades=prompt("¿Cuantas unidades de su "+tipoDeProducto+" desea ordenar?");
+		cantidadDeUnidades=parseInt(cantidadDeUnidades);
+		while(isNaN(cantidadDeUnidades)&&cantidadDeUnidades<1&&cantidadDeUnidades>50)
+		{
+			cantidadDeUnidades=prompt("Error.. Ingrese una cantidad valida. Solo disponemos de 50 unidades para su producto");
+		}
+		marcaDeProducto=prompt("Ingrese la marca de su "+tipoDeProducto);
+		while(marcaDeProducto!="Seagate"&&marcaDeProducto!="Western Digital"&&marcaDeProducto!="Kingston")
+		{
+			marcaDeProducto=prompt("Error.. Ingrese una marca valida (“Seagate”, “Western Digital” o “Kingston”)");
+		}
+		capacidadDisco=prompt("Ingrese la capacidad que quiere para su disco (250GB, 500GB, 1TB o 2TB");
+		while(capacidadDisco!="250GB"&&capacidadDisco!="500GB"&&capacidadDisco!="1TB"&&capacidadDisco!="2TB")
+		{
+			capacidadDisco=prompt("Error..Ingrese una capacidad válida (250GB, 500GB, 1TB o 2TB)");
+		}
+		switch(tipoDeProducto)//Bueno hasta acá llegué :(
+		{
+			case "HDD":
+			acumuladorHDD++;
+			break;
+			case "SSD":
+			acumuladorSSD++;
+			break;
+		}
+		contadorProductos++;
+	}
 }
